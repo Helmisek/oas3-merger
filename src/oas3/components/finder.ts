d7804cf57ff7c4d936ab21251f713499c411b5ce
helmisek <helmanss1@gmail.com>
2022-01-28 01:14:22 +0100

    feat: add the rest of component structures

--- src/oas3/components/finder.ts
+++ src/oas3/components/finder.ts
@@ -8,19 +8,60 @@ export interface OAS3ComponentData {
 export function extractOAS3ComponentData(specification: any): OAS3ComponentData {
     return Object.keys(specification).map(key => {
         switch (key) {
-            case 'securitySchemes': {
-                return {
-                    type: OAS3Component.securitySchemes,
-                    data: specification[key],
-                }
-            }
             case 'schemas': {
                 return {
                     type: OAS3Component.schemas,
                     data: specification[key],
                 }
             }
-            // TODO: add the rest here
+            case 'parameters': {
+                return {
+                    type: OAS3Component.parameters,
+                    data: specification[key],
+                }
+            }
+            case 'securitySchemes': {
+                return {
+                    type: OAS3Component.securitySchemes,
+                    data: specification[key],
+                }
+            }
+            case 'requestBodies': {
+                return {
+                    type: OAS3Component.requestBodies,
+                    data: specification[key],
+                }
+            }
+            case 'responses': {
+                return {
+                    type: OAS3Component.responses,
+                    data: specification[key],
+                }
+            }
+            case 'headers': {
+                return {
+                    type: OAS3Component.headers,
+                    data: specification[key],
+                }
+            }
+            case 'examples': {
+                return {
+                    type: OAS3Component.examples,
+                    data: specification[key],
+                }
+            }
+            case 'links': {
+                return {
+                    type: OAS3Component.links,
+                    data: specification[key],
+                }
+            }
+            case 'callbacks': {
+                return {
+                    type: OAS3Component.callbacks,
+                    data: specification[key],
+                }
+            }
             default:
                 throw new Error('Unrecognizable key found in provided specification.')
         }
